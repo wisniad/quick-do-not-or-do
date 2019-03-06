d9d30763a5aae0d94658e60778ae70a1f2dee113
Damian Wiśniewski <damian.wisniewski@asseco.pl>
2019-03-06 15:37:56 +0100

    Adding sample routing for login, app interface and quicky component

---
+++ src/app/login/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+  login() {
+    this.router.navigate(['/quicky']);
+  }
+}
